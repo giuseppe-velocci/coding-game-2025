d0facf30ec904c2bbfdbb4ccd9f0eb076c5592ce
Giuseppe Velocci <runnermean@gmail.com>
2025-04-04 22:19:34 +0200

    feat: tested validators

---
+++ OrderService/ProductService/Categories/CategoryValidator .cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+using Infrastructure;
+
+namespace ProductService.Categories
+{
+    public class CategoryValidator : BaseValidator<Category>
+    {
+        protected override void SetupValidation()
+        {
+            RuleFor(category => category.CategoryId)
+            .GreaterThan(-1).WithMessage("Category ID must be greater or equal than 0.");
+
+            RuleFor(category => category.Name)
+                .NotEmpty().WithMessage("Category name is required.")
+                .MaximumLength(100).WithMessage("Category name must not exceed 100 characters.");
+        }
+    }
+}
